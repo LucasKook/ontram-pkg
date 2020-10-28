4e6599afdc61b807b00c15ecb3c14334d1a5c9b7
lkook <lucasheinrich.kook@uzh.ch>
2020-10-28 09:22:51 +0100

    to theta, to gamma

--- R/helper.R
+++ R/helper.R
@@ -1,6 +1,17 @@
 # Internal for checks
+
+#' gamma to theta
+#' @examples
+#' .to_theta(c(-1, 1, 1))
 .to_theta <- function(gammas) {
   return(c(gammas[1], gammas[1] + cumsum(exp(gammas[-1]))))
+}
+
+#' theta to gamma
+#' @examples
+#' .to_gamma(.to_theta(c(-1, 1, 1)))
+.to_gamma <- function(thetas) {
+  return(c(thetas[1L], log(diff(thetas))))
 }
 
 #' @importFrom tensorflow tf
