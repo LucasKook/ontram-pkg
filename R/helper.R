c98297fdf735cd4556aee0fb4d47bdd56d4be7b9
lkook <lucasheinrich.kook@uzh.ch>
2021-11-08 13:51:27 +0100

    get weights by name

--- R/helper.R
+++ R/helper.R
@@ -22,3 +22,12 @@
                 "probit" = stop("Not implemented yet."))
   return(ret)
 }
+
+#' Get weights by name of layer
+#' @export
+get_weights_by_name <- function(m, name) {
+  layer_names <- unlist(lapply(m$layers, function(x) x$name))
+  layer_weights <- lapply(m$layers, function(x) x$weights)
+  idx <- which(layer_names == name)
+  as.vector(layer_weights[[idx]][[1]]$numpy())
+}
