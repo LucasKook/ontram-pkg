9e863660853cc3d4382f9fcec5a5784be5d92aef
lkook <lucasheinrich.kook@uzh.ch>
2021-11-19 14:24:00 +0100

    nop

--- demo/wine-k_ontram.R
+++ demo/wine-k_ontram.R
@@ -29,7 +29,7 @@ fit(m, x = list(INT, X, Z), y = Y, batch_size = nrow(wine), epoch = 10,
 tm <- Polr(rating ~ temp + contact + noise, data = wine)
 
 tmp <- get_weights(m)
-tmp[[1]][] <- .to_gamma(coef(as.mlt(tm))[1:4])
+tmp[[1]][] <- ontram:::.to_gamma(coef(as.mlt(tm))[1:4])
 tmp[[2]][] <- coef(tm)[1:2]
 tmp[[3]][] <- coef(tm)[3]
 set_weights(m, tmp)
