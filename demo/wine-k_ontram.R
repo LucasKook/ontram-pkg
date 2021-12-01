b249949e20dc46c090119d15162a3b3e664a382e
lkook <lucasheinrich.kook@uzh.ch>
2021-12-01 16:11:28 +0100

    rps example

--- demo/wine-k_ontram.R
+++ demo/wine-k_ontram.R
@@ -25,18 +25,26 @@ k_nll <- k_ontram_loss(ncol(Y))
 loss <- k_ontram_rps(ncol(Y))
 compile(m, loss = loss, optimizer = optimizer_adam(lr = 1e-2, decay = 0.001),
         metrics = c(metric_nll(ncol(Y))))
-fit(m, x = list(INT, X, Z), y = Y, batch_size = ncol(Y), epoch = 10,
+fit(m, x = list(INT, X, Z), y = Y, batch_size = ncol(Y), epoch = 1e3,
     view_metrics = FALSE)
 
 tm <- Polr(rating ~ temp + contact + noise, data = wine)
 
-tmp <- get_weights(m)
-tmp[[1]][] <- ontram:::.to_gamma(coef(as.mlt(tm))[1:4])
-tmp[[2]][] <- coef(tm)[1:2]
-tmp[[3]][] <- coef(tm)[3]
-set_weights(m, tmp)
-
 k_nll(k_constant(Y), m(list(INT, X, Z)))
 - logLik(tm) / nrow(wine)
 
-head(predict(m, list(INT, X, Z), type = "cumhaz"))
+head(predict(m, list(INT, X, Z), type = "distribution"))
+
+rps_polr <- function(y_true, y_pred) {
+  K <- ncol(y_true)
+  y_cum <- apply(y_true, 1, cumsum)
+  briers <- (y_pred[, 1:(K - 1), drop = FALSE] -
+               y_cum[, 1:(K - 1), drop = FALSE])^2
+  mean(apply(briers, 2, mean))
+}
+
+tm_preds <- predict(tm, newdata = wine[, !colnames(wine) == "rating"],
+                    type = "distribution")
+
+loss(k_constant(Y), m(list(INT, X, Z)))
+rps_polr(Y, tm_preds)
