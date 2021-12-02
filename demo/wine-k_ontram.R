a877cc9c82dc374566823de71314cb708d795109
lkook <lucasheinrich.kook@uzh.ch>
2021-12-02 15:06:24 +0100

    demo

--- demo/wine-k_ontram.R
+++ demo/wine-k_ontram.R
@@ -25,7 +25,7 @@ k_nll <- k_ontram_loss(ncol(Y))
 loss <- k_ontram_rps(ncol(Y))
 compile(m, loss = loss, optimizer = optimizer_adam(lr = 1e-2, decay = 0.001),
         metrics = c(metric_nll(ncol(Y)), metric_acc(ncol(Y)), metric_qwk(ncol(Y)),
-                    metric_k_auc(ncol(Y)), metric_))
+                    metric_k_auc(ncol(Y)), metric_binll(ncol(Y))))
 mh <- fit(m, x = list(INT, X, Z), y = Y, batch_size = floor(0.9 * ncol(Y)), epoch = 200,
     view_metrics = FALSE, validation_split = 0.1)
 plot(mh)
